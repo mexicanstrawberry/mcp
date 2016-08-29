86cceddc705024c41af9fc5f942ffcb0eb515bee
Morris Winkler <m.winkler@laydrop.com>
2016-08-30 01:54:04 +0200

    changed Http Server

--- web.go
+++ web.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -19,18 +18,31 @@ func StartHttpServer() {
 
 	var err error
 
-	addr := "0.0.0.0"
-
+	/*
+	* Load server address and port
+	 */
+	// load port
 	var port string
 	if port = os.Getenv("PORT"); len(port) == 0 {
 		port = "8080"
 	}
 
-	listenAddr := fmt.Sprintf("%s:%s", addr, port)
-	clog.Infoln("Server Listening", "address", listenAddr, "protocol")
+	// load server addr
+	var addr string
+	if addr = os.Getenv("CF_INSTANCE_ADDR"); len(addr) == 0 {
+
+		// CF_INSTANCE_ADDR is empty, set it to localhost
+		addr = "127.0.0.1:" + port
+
+	} else {
+		// CF_INSTANCE_ADDR is set
+		addr = ":" + port
+	}
+
+	clog.Infoln("Server Listening", "address", addr, "protocol")
 
 	http.HandleFunc("/", index)
-	err = http.ListenAndServe(listenAddr, nil)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		clog.Fatal(err)
 	}
