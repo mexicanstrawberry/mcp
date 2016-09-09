6d28007af122250a63796113854f922a29f8cdeb
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-09-09 22:05:37 +0200

    changing humidity darget

--- recipe/recipe.go
+++ recipe/recipe.go
@@ -79,7 +79,7 @@ func (so *SimpleOperation) LoadDummyRecipe() {
 
 		t := new(time.Time)
 		opT.Time = t.Add(time.Duration(i*interval) * time.Second)
-		opT.Value = 42 //rand.Float64() * 100
+		opT.Value = 50 //rand.Float64() * 100
 		so.Sensors["InsideHumidity"] = append(so.Sensors["InsideHumidity"], opT)
 
 		opT.Value = 23 //rand.Float64() * 100
